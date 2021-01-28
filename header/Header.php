fb262453c20e0040e05ae47cb1383ac3c3cced89
Aaron Vargas <el_iorimx@hotmail.com>
2021-01-28 16:07:37 -0600

    Carpetas, index y header

---
+++ header/Header.php
@@ -0,0 +1,23 @@
+<div class="header">
+
+    <div class="logoContainer">
+        <img src="" alt="" class="logo">
+    </div>
+
+    <nav class="menuContainer">
+        <ul>
+            <li>¿Quiénes somos?</li>
+            <li>Nuestros Servicios</li>
+            <li>Nuestros Clientes</li>
+            <li>Realiza tu cotización</li>
+            <li>Contáctanos</li>
+        </ul>
+    </nav>
+
+    <div class="socialContainer">
+        <a href="" class="facebook"><i class="fa fa-facebook-square"></i></a>
+        <a href="" class="instagram"><i class="fa fa-instagram"></i></a>
+        <a href="" class="whatsapp"><i class="fa fa-whatsapp "></i></a>
+    </div>
+
+</div>
