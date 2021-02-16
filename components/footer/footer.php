ef07ebf5040405b5008127e2e4227684f9a2ffb0
Isaac Ortiz <isaac.ortizse@gmail.com>
2021-02-16 12:01:22 -0600

    ADD: Footer Styles

--- components/footer/footer.php
+++ components/footer/footer.php
@@ -1,15 +1,23 @@
-<div class="footer">
+<div class="footer-container">
 
-    <div class="logo-container">
-        <img src="" alt="" class="logo">
+    <div class="footer-logo">
+        <picture loading="lazy">
+            <source srcset="assets/images/logo.webp" type="image/webp" loading="lazy" />
+            <source srcset="assets/images/logo.png" type="image/png" />
+            <img class="logo" src="assets/images/logo.png" title="Tecsan Logo" alt="Tecsan Logo">
+        </picture>
     </div>
 
-    <div class="menu-container">
+    <div class="footer-text">
         <p>Todos los derechos reservados © 2020</p>
     </div>
 
-    <div class="logo-container">
-        <img src="" alt="" class="logo">
+    <div class="footer-logo">
+        <picture loading="lazy">
+            <source srcset="assets/images/logo.webp" type="image/webp" loading="lazy" />
+            <source srcset="assets/images/logo.png" type="image/png" />
+            <img class="logo" src="assets/images/logo.png" title="Tecsan Logo" alt="Tecsan Logo">
+        </picture>
     </div>
 
 </div>
